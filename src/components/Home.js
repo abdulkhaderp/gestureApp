09ccd525b5767637b324eee7e6e85a9688a5e5b0
Abdul khader P <abdulkhaderp@gmail.com>
2022-07-09 17:48:07 +0530

    mail flow of speak and list

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,27 @@ import React, { useRef, useState, useEffect } from "react";
 import "../App.css";
 import Header from "./Header";
 import _Home from './../components/_Home';
+import Speak from "./Speak";
+import { gestureData } from '../config';
+import { useSpeechSynthesis } from 'react-speech-kit';
+
 
 export default function Home() {
-
+    const {speak} = useSpeechSynthesis();
+    const [listView, setListView] = useState(false);
     const [addGesture, setaddGesture] = useState(false);
+    const [speakMode, setSpeakMode] = useState(true);
     const [GestureMessage, setGestureMessage] = useState({ message: ""})
+    const [qSet, setQSet] = useState([]);
 
     const handleAddGesture = () => {
+        setSpeakMode(false);
         setaddGesture(true)
+    }
+
+    const handleSpeak =()=>{
+         setaddGesture(false);
+        setSpeakMode(true);
     }
 
     const handleChange = (e) => {
@@ -19,14 +32,38 @@ export default function Home() {
         console.log(GestureMessage);
       }
 
+    const showList =()=>{
+        setListView(true);
+        setSpeakMode(false);
+        setaddGesture(false);
+        //just fetch all qstn from local storage
+        let ar=[];
+        Object.keys(gestureData).forEach((i)=>{
+            ar.push(gestureData[i]);
+        })
+        let qstns=[];
+        ar.forEach((q)=>{
+            if(localStorage.getItem(q)){
+                qstns.push(localStorage.getItem(q));
+            }
+        })
+        setQSet(qstns);
+
+    }  
+
+    const playQstn =(i)=>{
+        console.log(i);
+        speak({text:i})
+    }
+
     return (
         <>
             <Header></Header>
             <div className="container p-4">
                 <div className="d-flex justify-content-around">
+                    <button type="button" onClick={handleSpeak}  class="btn btn-dark"><i className="fas fa-heart"></i></button>
                     <button type="button" onClick={handleAddGesture} class="btn btn-primary"><i className="fas fa-plus"></i></button>
-                    <button type="button" class="btn btn-dark"><i className="fas fa-heart"></i></button>
-                    <button type="button" class="btn btn-success"><i className="fas fa-list"></i></button>
+                    <button type="button" onClick={showList} class="btn btn-success"><i className="fas fa-list"></i></button>
                 </div>
             </div>
             <hr />
@@ -51,6 +88,37 @@ export default function Home() {
                     </div>
                 </>}
 
+                { speakMode && <><div className="row justify-content-center">
+                    <div className="col-12">
+                        <h5>Speak Page- title to be removed</h5>
+                        <div class="container-fluid">
+                            <Speak></Speak>
+                        </div>
+                    </div>
+                    
+
+                </div>
+                    
+                </>}
+
+                {
+                    listView && <><div className="row justify-content-center">
+                    <div className="col-12">
+                        <h5>List Page- title to be removed</h5>
+                        <div class="container-fluid">
+                            {qSet.map((i)=>{
+                                return (
+                                    <><li>{i}</li> <input type="button" value="play" onClick={()=> playQstn(i)}></input></>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    
+
+                </div>
+                    
+                </>
+                }
             </div>
         </>
 
